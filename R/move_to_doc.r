ae0169958d475cf19a314089b17537c06492bb76
André Calero Valdez <andrecalerovaldez@gmail.com>
2019-10-09 21:30:04 +0200

    index.html

--- R/move_to_doc.r
+++ R/move_to_doc.r
@@ -1,2 +1,2 @@
-file.copy("R/Analysis.html", to="docs/Analysis.html", overwrite = TRUE)
+file.copy("R/Analysis.html", to="docs/index.html", overwrite = TRUE)
 file.copy("R/Analysis_files/", to="docs/", overwrite = TRUE, recursive = T)
